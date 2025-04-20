0f659013d86e627754ff7d05f9606e1c2079ab08
me-safty <me.a.safty@gmail.com>
2025-04-20 20:17:12 +0200

    fix: ts error

--- app/too-fast/page.tsx
+++ app/too-fast/page.tsx
@@ -1,4 +1,4 @@
-export const Page = () => {
+const Page = () => {
   return (
     <main className="root-container flex min-h-screen flex-col items-center justify-center">
       <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
--- next.config.ts
+++ next.config.ts
@@ -18,12 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 }
 
 export default nextConfig
